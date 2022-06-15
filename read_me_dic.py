77929ab1c8c17053e151809d78ccd7419245aaed
Jomoartz <105460959+Jomoartz@users.noreply.github.com>
2022-06-15 06:40:01 +0100

    Update read_me_dic.py

--- read_me_dic.py
+++ read_me_dic.py
@@ -1,21 +1,30 @@
-with open('story.txt') as f:
-    content = f.readlines()
-    contents=content.copy()
-    f.close()
-   
-   
-contents
-string_mker=(' '.join(contents))
-string=string_mker.split(' ')
+def read_file_content(filename):
+    with open(filename) as f:
+        content = f.readlines()
+        contents=content.copy()
+        f.close()
+        return contents
+       
+       
+#in COUNT_WORDS FUNCT.(read_file_content() function is used without 'import" because both functs are in same file.)
+def count_words(filename):
+    texts=read_file_content(filename)
+    string_mker=(' '.join(texts))
+    string=string_mker.split(' ')
+    dic={
+    " ":" ",}
+    msg=string
+    for words in msg:
+        word_count=msg.count(words)
+        dic[words]=word_count
+    return(dic)
+    
+    
 
-dic={
-" ":" ",}
 
-msg=string
-for words in msg:
-    word_count=msg.count(words)
-    dic[words]=word_count
-return(dic)
+#trial:
+print (count_words("story.txt"))
+    
    
 
 
